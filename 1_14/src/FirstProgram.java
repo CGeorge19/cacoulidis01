38bbefbdfdb4eb4b1362584605f8dcf3863a8a79
CGeorge19 <yorgicacoulidis@gmail.com>
2020-01-14 19:10:41 -0500

    a really long comment

---
+++ 1_14/src/FirstProgram.java
@@ -0,0 +1,17 @@
+
+public class FirstProgram 
+{
+
+	public static void main(String[] args) 
+	{
+		int age = 13;
+		double weight = 123.345678;
+		
+		System.out.printf("Hello everyone!%n");
+		// %n is the universal new line format specifier across platforms
+		
+		System.out.printf("Hello everyone!%nI am %d years old!%nI weigh %.2f", age, weight);
+		
+		
+	}
+}
---
+++ 1_14/src/SecondProgram.java
@@ -0,0 +1,6 @@
+public class SecondProgram
+{
+	public static void main(String[] args) 
+	{
+	}
+}
